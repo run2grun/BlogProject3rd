579a0aebd3104b59922976daaed9956cf9e27202
run2grun <run2grun@gmail.com>
2019-07-28 17:49:20 +0900

    first blog

--- blogProject/settings.py
+++ blogProject/settings.py
@@ -20,10 +20,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'hjs^n(svz4rklp)3r=nrhqx!^fe@ndrn*h3xfsp3b3mnoe#_)9'
+# SECRET_KEY = ''
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '"YOUR_SECREY_KEY")YOUR_SECREY_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
 ALLOWED_HOSTS = []
 
--- blogapp/urls.py
+++ blogapp/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('blog/<int:blog_id>', views.detail, name='detail'),
-    path('blog/new', views.new, name='new'),
-    path('blog/create', views.create, name='create'),
-    path('blog/edit/<int:blog_id>', views.edit, name='edit'),
-    path('blog/update/<int:blog_id>', views.update, name='update'),
-    path('blog/delete/<int:blog_id>', views.delete, name='delete'),
+    path('<int:blog_id>', views.detail, name='detail'),
+    path('new', views.new, name='new'),
+    path('create', views.create, name='create'),
+    path('edit/<int:blog_id>', views.edit, name='edit'),
+    path('update/<int:blog_id>', views.update, name='update'),
+    path('delete/<int:blog_id>', views.delete, name='delete'),
 ]
